a2679afa916f11392aa8cb82f98f9cfb93f3602b
Poloflv <kevinpolo316@gmail.com>
2023-11-23 17:38:58 -0500

    remove data.services

--- src/app/data.services.ts
+++ src/app/data.services.ts
@@ -1,27 +1,27 @@
-import {HttpClient} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Persona } from './persona.model';
+// import {HttpClient} from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Persona } from './persona.model';
 
 
-@Injectable()
-export class DataServices{
-    constructor(private httpClient: HttpClient){}
+// @Injectable()
+// export class DataServices{
+//     constructor(private httpClient: HttpClient){}
 
 
-    cargarPersonas(){
-        return this.httpClient.get('https://listado-personas-f8449-default-rtdb.firebaseio.com/datos.json');
-    }
+//     cargarPersonas(){
+//         return this.httpClient.get('https://listado-personas-f8449-default-rtdb.firebaseio.com/datos.json');
+//     }
 
 
-    //Guardar Personas
-    guardarPersonas(personas: Persona[]){
-        this.httpClient.put('https://listado-personas-f8449-default-rtdb.firebaseio.com/datos.json', personas )
-        .subscribe(
-            response => console.log("resultado de guardar las Personas " + response),
-            error => console.log("Error al guardar Personas: " + error)
+//     //Guardar Personas
+//     guardarPersonas(personas: Persona[]){
+//         this.httpClient.put('https://listado-personas-f8449-default-rtdb.firebaseio.com/datos.json', personas )
+//         .subscribe(
+//             response => console.log("resultado de guardar las Personas " + response),
+//             error => console.log("Error al guardar Personas: " + error)
             
-        );
-    }
+//         );
+//     }
 
 
     /*
@@ -36,4 +36,4 @@ export class DataServices{
         )
     }
     */
-}
+// }
